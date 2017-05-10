b4ee8694e933eebdf5cb7ad575073ba24d9d9329
Blue <ericmager@gmx.de>
2017-05-11 00:20:59 +0200

    i keep forgetting this damn formatting

--- bot.js
+++ bot.js
@@ -11,19 +11,19 @@ var Twitter = new TwitterPackage(secret);
 
 
 function tweetStuff() {
-var text = fs.readFileSync("./tweets.txt").toString('utf-8');
-var tweetArray = textByLine = text.split("\n")
+  var text = fs.readFileSync("./tweets.txt").toString('utf-8');
+  var tweetArray = textByLine = text.split("\n")
 
-console.log("Posting: "+tweetArray[Math.floor(Math.random() * tweetArray.length)])
+  console.log("Posting: "+tweetArray[Math.floor(Math.random() * tweetArray.length)])
 
-Twitter.post('statuses/update', {status: tweetArray[Math.floor(Math.random() * tweetArray.length)]},  function(error, tweet, response){
-  if(error){
-    console.log(error);
-  }
-  console.log(tweet);
-  console.log(response);
-});
-setTimeout(tweetStuff,3600000)
+    Twitter.post('statuses/update', {status: tweetArray[Math.floor(Math.random() * tweetArray.length)]},  function(error, tweet, response){
+      if(error){
+         console.log(error);
+      }
+     console.log(tweet);
+     console.log(response);
+  });
+  setTimeout(tweetStuff,3600000)
 }
 
 tweetStuff()
